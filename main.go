a9adbce62f8728c52ca5f672526ecfe63035ff9e
Justin Beausoleil <beausoleil.justin@gmail.com>
2021-08-10 21:48:23 -0600

    Iterating over maps

--- main.go
+++ main.go
@@ -4,15 +4,21 @@ import "fmt"
 
 func main () {
 	//var colors map[string]string // initialize with zero value (empty map)
-
 	//colors := make(map[string]string) // create a map with no values inside where all keys and values are string;
 
 	colors := map[string]string{ // [keys]values
 		"red":"#ff0000",
 		"green":"#4bf745",
+		"white":"#ffffff",
 	}
-	colors["white"] = "#ffffff" // add to map
-	delete(colors, "white") // delete from map
+	//colors["white"] = "#ffffff" // add to map
+	//delete(colors, "white") // delete from map
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap (c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }

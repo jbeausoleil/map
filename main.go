18c952dc6db6db8eecdde448af653e7bd0f22e13
Justin Beausoleil <beausoleil.justin@gmail.com>
2021-08-10 21:38:10 -0600

    Manipulating maps

--- main.go
+++ main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main () {
+	//var colors map[string]string // initialize with zero value (empty map)
+
+	//colors := make(map[string]string) // create a map with no values inside where all keys and values are string;
+
 	colors := map[string]string{ // [keys]values
 		"red":"#ff0000",
 		"green":"#4bf745",
 	}
+	colors["white"] = "#ffffff" // add to map
+	delete(colors, "white") // delete from map
+
 	fmt.Println(colors)
 }
